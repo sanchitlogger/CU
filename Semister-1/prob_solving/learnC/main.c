59257b72d0e9c8df533b366f29cdc71246fe9de2
Sanchit Gupta <sanchitlogger@outlook.com>
2023-09-07 14:55:37 +0000

    files made by vs code

--- Semister-1/prob_solving/learnC/main.c
+++ Semister-1/prob_solving/learnC/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(){
+	char nm;
+	printf("Enter Name :");
+	scanf("%s",&nm);
 	printf("Namaste!");
+	return 0;
 }
---
+++ build/CMakeFiles/learnC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnC.dir/main.c.o"
+  "CMakeFiles/learnC.dir/main.c.o.d"
+  "learnC"
+  "learnC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/learnC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
